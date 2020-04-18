71963f0c988337eecc52d733801188e889a1001b
KHAYOUSSEF MOHAMED <mohamed.khayoussef17@gmail.com>
2020-04-18 15:00:33 +0200

    choix du thème
    en appuyant sur le button "Jouer un quiz" on peut choisir le thème qui contient le nom et le nom de quiz

--- back-end/app/models/theme.model.js
+++ back-end/app/models/theme.model.js
@@ -4,5 +4,6 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('Theme', {
   id: Joi.number(), 
   name: Joi.string().required(),
+  quizNumber: Joi.number()
   //image: Joi.string().allow(''),
 })
